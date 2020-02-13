9023ce4e16650220faf4f47d58e200a6d00b6f78
0v3rride <35246589+0v3rride@users.noreply.github.com>
2020-02-13 11:50:50 -0600

    Update ShareHunter_Multithreaded.ps1
    Added -SubnetList @(x.x.x.x/x, x.x.x.x/x) flag which uses the Get-IPs function to build a list of IPs to target during share enumeration.

--- PowerShell/ShareHunter_Multithreaded.ps1
+++ PowerShell/ShareHunter_Multithreaded.ps1
@@ -51,12 +51,15 @@ function Invoke-ShareHunter {
         Invoke-ShareHunter -Hostlist .\targetlist.txt -Enumerate Files -ShowProgress -FileNames "web.config", "machine.config" -FileValues "machinekey" -Threads 20
     #>
 
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = "SubnetList")]
     param (
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false, ParameterSetName = "HostList", HelpMessage = "The fully qualified path to the text file containing the SMB hosts delimited by a new line")]
         [string]$HostList,
 
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false, ParameterSetName = "SubnetList", HelpMessage = "Provide a subnet or a list of subnets to build a list of IPs from by using the builtin IP enumerator")]
+        [string[]]$SubnetList,
+
+        [Parameter(Mandatory = $true, HelpMessage = "Enumerate the permissions on the top-level of the share itself or enumerate the files within each share")]
         [ValidateSet("Permissions", "Files")]
         [string]$Enumerate,
 
@@ -77,6 +80,87 @@ function Invoke-ShareHunter {
         [ValidateRange(1, 300)]
         [int]$Threads = 50
     )
+
+    #https://gallery.technet.microsoft.com/scriptcenter/PowerShell-Subnet-db45ec74
+    function Get-IPs { 
+ 
+        Param( 
+            [Parameter(Mandatory = $true)] 
+            [array] $Subnets 
+        ) 
+ 
+        foreach ($subnet in $subnets) { 
+         
+            #Split IP and subnet 
+            $IP = ($Subnet -split "\/")[0] 
+            $SubnetBits = ($Subnet -split "\/")[1] 
+         
+            #Convert IP into binary 
+            #Split IP into different octects and for each one, figure out the binary with leading zeros and add to the total 
+            $Octets = $IP -split "\." 
+            $IPInBinary = @() 
+            foreach ($Octet in $Octets) { 
+                #convert to binary 
+                $OctetInBinary = [convert]::ToString($Octet, 2) 
+                 
+                #get length of binary string add leading zeros to make octet 
+                $OctetInBinary = ("0" * (8 - ($OctetInBinary).Length) + $OctetInBinary) 
+ 
+                $IPInBinary = $IPInBinary + $OctetInBinary 
+            } 
+            $IPInBinary = $IPInBinary -join "" 
+ 
+            #Get network ID by subtracting subnet mask 
+            $HostBits = 32 - $SubnetBits 
+            $NetworkIDInBinary = $IPInBinary.Substring(0, $SubnetBits) 
+         
+            #Get host ID and get the first host ID by converting all 1s into 0s 
+            $HostIDInBinary = $IPInBinary.Substring($SubnetBits, $HostBits)         
+            $HostIDInBinary = $HostIDInBinary -replace "1", "0" 
+ 
+            #Work out all the host IDs in that subnet by cycling through $i from 1 up to max $HostIDInBinary (i.e. 1s stringed up to $HostBits) 
+            #Work out max $HostIDInBinary 
+            $imax = [convert]::ToInt32(("1" * $HostBits), 2) - 1 
+ 
+            $IPs = @() 
+ 
+            #Next ID is first network ID converted to decimal plus $i then converted to binary 
+            For ($i = 1 ; $i -le $imax ; $i++) { 
+                #Convert to decimal and add $i 
+                $NextHostIDInDecimal = ([convert]::ToInt32($HostIDInBinary, 2) + $i) 
+                #Convert back to binary 
+                $NextHostIDInBinary = [convert]::ToString($NextHostIDInDecimal, 2) 
+                #Add leading zeros 
+                #Number of zeros to add  
+                $NoOfZerosToAdd = $HostIDInBinary.Length - $NextHostIDInBinary.Length 
+                $NextHostIDInBinary = ("0" * $NoOfZerosToAdd) + $NextHostIDInBinary 
+ 
+                #Work out next IP 
+                #Add networkID to hostID 
+                $NextIPInBinary = $NetworkIDInBinary + $NextHostIDInBinary 
+                #Split into octets and separate by . then join 
+                $IP = @() 
+                For ($x = 1 ; $x -le 4 ; $x++) { 
+                    #Work out start character position 
+                    $StartCharNumber = ($x - 1) * 8 
+                    #Get octet in binary 
+                    $IPOctetInBinary = $NextIPInBinary.Substring($StartCharNumber, 8) 
+                    #Convert octet into decimal 
+                    $IPOctetInDecimal = [convert]::ToInt32($IPOctetInBinary, 2) 
+                    #Add octet to IP  
+                    $IP += $IPOctetInDecimal 
+                } 
+ 
+                #Separate by . 
+                $IP = $IP -join "." 
+                $IPs += $IP 
+ 
+                 
+            } 
+            
+            return $IPs 
+        } 
+    }
 
     $JobBlock = {
         [CmdletBinding()]
@@ -236,7 +320,7 @@ function Invoke-ShareHunter {
                 try {
                     foreach ($AccessObject in (Get-Acl -Path "\\$($TargetHost)\$($TargetShare)" -ErrorAction Stop).Access) {
                         $Result = "" | Select-Object -Property UNCPath, Principal, Verb, NTFSPermissions;
-                        $Result.UNCPath = "\\$($TargetHost)\$($TargetShare)";
+                        $Result.UNCPath = ("\\{0}\{1}" -f [System.Net.Dns]::GetHostEntry($TargetHost).HostName, $TargetShare);
                         $Result.Principal = $AccessObject.IdentityReference;
                         $Result.Verb = $AccessObject.AccessControlType;
                         $Result.NTFSPermissions = $AccessObject.FileSystemRights;
@@ -291,9 +375,16 @@ function Invoke-ShareHunter {
 
 
     # Preliminary setup 
-    $Targets = (Get-Content -Path $HostList);
+    $Targets = $null;
     $Count = 0;
     $Credential = $null;
+
+    if ($HostList -and !($SubnetList)) {
+        $Targets = (Get-Content -Path $HostList);
+    }
+    elseif ($SubnetList -and !($HostList)){
+        $Targets = Get-IPs -Subnets $SubnetList
+    }
 
     if ($Password) {
         $SecPassword = (ConvertTo-SecureString $Password -AsPlainText -Force); 
